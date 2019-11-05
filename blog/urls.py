85ceb2e567cc992df65ecb1ab3865f627ba7f8e2
ap18047 <ap18047@azec.co.jp>
2019-11-05 11:32:28 +0900

    My

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import path,include, re_path
 from . import views
 from django.shortcuts import render, get_object_or_404
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-    path(r'^post/new/$', views.post_new, name='post_new'),
-    path(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    re_path(r'^post/new/$', views.post_new, name='post_new'),
+    re_path(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+
+
+
 ]
