0ad55e7ea0649db3f475e429a395e9a0004ffc3f
ap18047 <ap18047@azec.co.jp>
2019-11-05 10:26:50 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,6 @@ from django.shortcuts import render, get_object_or_404
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    path(r'^post/new/$', views.post_new, name='post_new'),
+    path(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
