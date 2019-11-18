6da5efbaf69b6aa387159c4ebe4aaffe066902da
ap18047 <ap18047@azec.co.jp>
2019-11-18 11:02:02 +0900

    new new

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,11 @@ from django.shortcuts import render, get_object_or_404
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    re_path(r'^index', views.index, name='index'),
     re_path(r'^post/new/$', views.post_new, name='post_new'),
     re_path(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    re_path(r'^China', views.China, name='China'),
+    re_path(r'^Japan', views.Japan, name='Japan'),
 
 
 
--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,13 @@ def post_detail(request, pk):
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+def index(request):
+    return render(request, 'blog/index.html')
+def China(request):
+    return render(request, 'blog/China.html')
+def Japan(request):
+    return render(request, 'blog/Japan.html')
+
 
 def post_new(request):
     if request.method == "POST":
